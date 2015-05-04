fd33914425b59063bb0198a3d17d756f505f376d
bmordan <bernardmordan@gmail.com>
2015-05-04 14:26:15 +0100

    git this

---
+++ README.md
@@ -0,0 +1,5 @@
+#TABLEGIT
+###Keeping track of actual developers and actual client's projects
+
+####Set this project up
+You need `node` installed. To start the server clone this project and type `npm i` then start the server with `npm start`
---
+++ server.js
@@ -0,0 +1,39 @@
+var Hapi = require('hapi')
+var Good = require('good')
+var Routes = require('./routes/routes')
+var server = new Hapi.Server()
+server.connection({
+  host: 'localhost',
+  port: '3000'
+})
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  relativeTo: __dirname,
+  path: './views',
+  layoutPath: './views/layouts',
+  layout: 'default'
+})
+server.register({
+  register: Good,
+  options: {
+    reporters: [{
+      reporter: require('good-console'),
+      events: {
+        response: '*',
+        log: '*'
+      }
+    }]
+  }
+}, onError)
+server.route(Routes.index)
+server.route(Routes.assets)
+server.start(onStartUp)
+
+function onStartUp () {
+  console.log('Hapi serving on ' + server.info.port)
+}
+function onError (err) {
+  if (err) throw console.error(err)
+}
