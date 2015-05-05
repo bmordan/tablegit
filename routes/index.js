a43b0a498b1a1d4ff9cedb97b7dfbdbc374668fa
bmordan <bernardmordan@gmail.com>
2015-05-05 23:08:58 +0100

    octonode library token auth and basic api calls working

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var request = require('request')
 var config = require('config')
+var github = require('octonode')
+
+var client = github.client(config.git.token)
 
 exports.render = {
   method: 'GET',
   path: '/',
   handler: function (req, res) {
-    res.view('index')
+    client.get('issues', {}, function (err, status, body, headers){
+      console.log(body)
+      res.view('index', {projects: body})
+    })
   }
 }
